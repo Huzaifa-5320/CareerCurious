a49e3b584f3d34cc3f4cea374af3708c87f37d14
Muhammad Anas Sohail <115977114+muhammadanascode@users.noreply.github.com>
2024-06-11 17:24:16 +0500

    updated module-info
    added sql for required modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.jobportal {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens com.jobportal to javafx.fxml;
     opens com.jobportal.controller to javafx.fxml;
