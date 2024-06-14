2ae0338efd35429e5cf56476abf5a33f37ac3d43
Muhammad Anas Sohail <115977114+muhammadanascode@users.noreply.github.com>
2024-06-14 20:38:44 +0500

    login feature
    Login feature has been added in Login controller to verify the existing users

--- src/main/java/com/jobportal/controller/Login.java
+++ src/main/java/com/jobportal/controller/Login.java
@@ -1,11 +1,13 @@
 package com.jobportal.controller;
 
+import com.jobportal.services.UserService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -21,21 +23,13 @@ public class Login {
     private PasswordField password;
 
     @FXML
-    void loginBtn(ActionEvent event) {
+    private Label errormsg;
 
-        String userEmail = email.getText();
-        String userPassword = password.getText();
-        System.out.println("User Email:" + userEmail);
-        System.out.println("Password:" + userPassword);
-        email.clear();
-        password.clear();
-
-    }
 
     @FXML
-    void navigateToSignUp(ActionEvent event)throws IOException {
+    void navigateToSignUp(ActionEvent event) throws IOException {
         //navigate to SignUp page
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/jobportal/Signup.fxml")) ;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/jobportal/Signup.fxml"));
         Parent root = loader.load();
         // Get the stage and set the new scene
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -43,6 +37,33 @@ public class Login {
         stage.setScene(scene);
         stage.show();
 
+    }
+
+    @FXML
+    void loginBtn(ActionEvent event) {
+
+        //extracting text
+        String userEmail = email.getText();
+        String userPassword = password.getText();
+
+        //creating object for services
+        UserService service = new UserService();
+
+        //checking for valid credentials
+        if (!service.loginVerification(userEmail, userPassword)) {
+
+            System.out.println("in error");
+            //setting error message
+            errormsg.setText("* Invalid Credentials");
+
+            //clearing fields
+            email.clear();
+            password.clear();
+
+            return;
+        }
+        errormsg.setText("");
+        System.out.println("Congratulations you are logged in");
     }
 
 }
