078744684cca36f355d287e0dbcafb7292a5fb2b
Muhammad Anas Sohail <115977114+muhammadanascode@users.noreply.github.com>
2024-06-16 16:55:50 +0500

    navigation to form ui
    A method has been created to navigate to a form through which we can create a job post

--- src/main/java/com/jobportal/controller/Home.java
+++ src/main/java/com/jobportal/controller/Home.java
@@ -2,7 +2,14 @@ package com.jobportal.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Home {
 
@@ -11,12 +18,24 @@ public class Home {
 
     @FXML
     void jobPostForm(ActionEvent event) {
-
+        try {
+            // Navigate to Post page
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/jobportal/Post.fxml"));
+            Parent root = loader.load();
+            // Get the stage and set the new scene
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            // Log the exception (use a proper logging framework in a real application)
+            System.out.println("Exception occurred while loading Post.fxml");
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void search(ActionEvent event) {
-
+        // Your search logic here
     }
-
 }
