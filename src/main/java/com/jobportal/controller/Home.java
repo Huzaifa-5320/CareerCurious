1a0211d46f8507320adb496032fe62ea8f724212
Muhammad Anas Sohail <115977114+muhammadanascode@users.noreply.github.com>
2024-06-16 15:49:04 +0500

    Home page
    home page view has been designed which consists of simple navbar search field and job list and their description . Along with this home page controller has been designed which doesn't do anything right now

---
+++ src/main/java/com/jobportal/controller/Home.java
@@ -0,0 +1,22 @@
+package com.jobportal.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Home {
+
+    @FXML
+    private TextField searchField;
+
+    @FXML
+    void jobPostForm(ActionEvent event) {
+
+    }
+
+    @FXML
+    void search(ActionEvent event) {
+
+    }
+
+}
